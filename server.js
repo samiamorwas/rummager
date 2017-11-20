fd46aabe5d2ad18fd3d74abbe1785fe535fe6f07
Max Haarhaus <samiamorwas@gmail.com>
2017-11-20 00:57:44 -0500

    Force ssl.

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const app = express();
 // Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// If an incoming request uses a protocol other than HTTPS,
+// redirect that request to the same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
